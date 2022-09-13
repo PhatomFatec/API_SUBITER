0d8291ae6f254d34f4109111c31637b48ce9b78e
Lucas Guiraldelli <lucas.guiraldelli@outlook.com>
2022-09-13 22:26:13 +0000

    Tabela crud produtos

--- front-subiter/src/views/ProdutosView.vue
+++ front-subiter/src/views/ProdutosView.vue
@@ -4,24 +4,35 @@
       <SearchComponent ph="Pesquisar" />
       <CriarButtonComponent txt="CRIAR PRODUTO" @click.native="formProduto()" />
     </div>
+    <FormProduto />
+    <ProdutosView/>
   </div>
 </template>
 
 <script>
-import SearchComponent from '@/components/SearchComponent.vue';
-import CriarButtonComponent from '@/components/CriarButtonComponent.vue';
+import SearchComponent from "@/components/SearchComponent.vue";
+import CriarButtonComponent from "@/components/CriarButtonComponent.vue";
+import FormProduto from "@/components/FormProduto.vue";
+import ProdutosView from "@/components/ProdutosComponent.vue"
 
 
 export default {
-  name: 'ProdutosComponents',
+  name: "ProdutosComponents",
   components: {
     SearchComponent,
     CriarButtonComponent,
+    FormProduto,
+    ProdutosView,
   },
   methods: {
     formProduto() {
-      alert("produto")
-    }
-  }
-}
+      var form = document.getElementById("modal")
+      form.style.display = "flex"
+    },
+    closeModal() {
+      var form = document.getElementById("modal")
+      form.style.display = "flex"
+    },
+  },
+};
 </script>
