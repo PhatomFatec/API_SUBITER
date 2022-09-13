86df416d6a07346c1fa5f76eadb79e14152ccd07
Marcus Vinícius <marcus.varocha@gmail.com>
2022-09-13 22:30:40 +0000

    produtos view

---
+++ front-subiter/src/components/ProdutosComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+    <h2>Lista de acesso aos Produtos</h2>
+    <table>
+      <tr>
+        <th>Código</th>
+        <th>Título</th>
+        <th>Cliente</th>
+        <th>Criação</th>
+        <th>Status</th>
+      </tr>
+      <tr>
+        <td>#18723</td>
+        <td>Lorem ipsum</td>
+        <td>Lorem ipsum</td>
+        <td>Há 10 dias</td>
+        <td>Em andamento</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ProdutosView',
+}
+</script>
