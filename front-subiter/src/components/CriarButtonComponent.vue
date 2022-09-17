da37e8896c9b7d4c94b5163d1d57129bc803c857
Marcus Rocha <marcus.varocha@gmail.com>
2022-09-17 20:52:38 -0300

    css adjustments

--- front-subiter/src/components/CriarButtonComponent.vue
+++ front-subiter/src/components/CriarButtonComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <button>
-        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none"
+        <!-- <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none"
             stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
             <line x1="12" y1="5" x2="12" y2="19"></line>
             <line x1="5" y1="12" x2="19" y2="12"></line>
-        </svg>
+        </svg> -->
         <p>{{txt}}</p>
     </button>
 </template>
