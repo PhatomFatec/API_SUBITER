704f16ef1bf4f21b5f0bc2df15d0ddb37957714a
Lucas Guiraldelli <lucas.guiraldelli@outlook.com>
2022-09-18 01:48:25 +0000

    Correcao de camps do get

--- front-subiter/src/components/AgendamentosComponent.vue
+++ front-subiter/src/components/AgendamentosComponent.vue
@@ -4,13 +4,19 @@
         <table>
             <tr>
                 <th>Código</th>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Razão Social</th>
-                <th>Telefone</th>
+                <th>Horario</th>
+                <th>Endereço</th>
+                <th>Cidade</th>
+                <th>CEP</th>
+                <th>Estado</th>
             </tr>
             <tr v-for="(agendamento, index) in agendamentos" v-bind:key="index">
                 <td>{{agendamento.id}}</td>
+                <td>{{agendamento.horario}}</td>
+                <td>{{agendamento.endereco}}</td>
+                <td>{{agendamento.cidade}}</td>
+                <td>{{agendamento.cep}}</td>
+                <td>{{agendamento.estado}}</td>
             </tr>
         </table>
     </div>
--- front-subiter/src/main.js
+++ front-subiter/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'http://localhost:8090'
 
 Vue.config.productionTip = false
 
