077bba8f59b0f32a560743bc4ceab25b03e6c59c
Lucas Guiraldelli <lucas.guiraldelli@outlook.com>
2022-10-06 01:47:01 +0000

    AS-36 Login screen

--- front-subiter/src/views/LoginView.vue
+++ front-subiter/src/views/LoginView.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="login">
-    <div class="login-esquerda"></div>
-    <div class="login-direita"></div>
+    <div class="login-esquerda">
+      <form action="">
+        <img src="@/assets/logo_Subiter_sn.png" alt="" />
+        <h1>Subiter</h1>
+        <h2>Log-in</h2>
+        <input type="text" placeholder="User" />
+        <input type="password" placeholder="Password" />
+        <button>Entrar</button>
+      </form>
+      <p>Alcance o invisível, Subiter</p>
+    </div>
+    <div class="login-direita">
+      <img src="@/assets/Computer login-bro.png" alt="" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style scoped>
+@import "@/sass/pages/login.css";
+</style>
