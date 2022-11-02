43a4f93e08ce6d606c3d966ecf84a9f35b27f095
Marcus Vinícius <marcus.varocha@gmail.com>
2022-11-02 00:03:06 +0000

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: [
+//     'vuetify'
+//   ]
+// })
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: {
+        port: process.env.GITPOD_WORKSPACE_ID ? 443 : undefined,
+      }
+    }
+  }
 })
